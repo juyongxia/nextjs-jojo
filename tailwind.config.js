5d0b10ab17a378a3c8e37375bd78aa2a211b009e
Juyongxia <yongxia.ju@iherb.com>
2022-03-01 16:23:25 +0800

    remove some tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  purge: [
-    "./pages/**/*.tsx",
-    "./pages/**/*.js",
-    "./pages/**/*.ts",
-    "./components/**/*.tsx",
-    "./components/**/*.js",
-    "./components/**/*.ts",
-  ],
   theme: {
     extend: {},
   },
